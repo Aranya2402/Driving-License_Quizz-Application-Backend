6d8f43b0c85547751ee42efa860fceccee1762fd
Aranya2402 <aranya24arii@gmail.com>
2024-04-05 20:38:00 +0530

    Dashboard_backend

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const app = require("./src/app");
-const port = 3000;
 
-app.listen( port, () => console.log("Server is running"));
+const app = require("./src/app");
+const port = process.env.PORT || 5000;
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
+// C:\Users\arany\OneDrive\Desktop\backend\src\app.js,C:\Users\arany\OneDrive\Desktop\backend\src\modules\ExamDashboard\ExamDash.js
--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const app = express();
-const authController = require('./modules/auth/auth-controller');
+// const express = require('express');
+// const app = express();
+// const authController = require('./modules/auth/auth-controller');
 
-app.use(express.json());
+// app.use(express.json());
 
-app.post('/auth/sign-in', authController.signInUser);
+// app.post('/auth/sign-in', authController.signInUser);
 
-app.get('/', (req, res) => {
-    res.send("hellllo")
-})
+// app.get('/', (req, res) => {
+    // res.send("hellllo")
+// })
 
 // 
 
-module.exports = app;
+// module.exports = app;
+
+// app.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ExamDash = require('./modules/ExamDashboard/ExamDash');
+
+const app = express();
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/exam-dashboard/exams', ExamDash);
+
+module.exports = app; // Export the app instance
+
