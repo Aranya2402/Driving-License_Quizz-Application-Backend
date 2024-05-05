ac880467e98d872afa5da18f87b53ec668174b61
SharujaB25 <sharuja.balendrakumar@gmail.com>
2024-05-05 19:21:15 +0530

    Authentication

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const app = require("./src/app");
+const mongoose = require("mongoose");
 const port = 3000;
+
+mongoose.connect('mongodb://localhost:27017/onlinedrivingquiz')
+    .then(r => {
+        console.log("MongoDB connected")
+    })
+    .catch(err => {
+        console.log("Error connecting mongodb", err);
+    })
 
 app.listen( port, () => console.log("Server is running"));
