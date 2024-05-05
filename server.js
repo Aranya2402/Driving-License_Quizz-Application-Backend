7a40d9fd72c4a28bdc13412c9131d6451e22f31c
vimalehaan <vimalehaan2312@mail.com>
2024-05-05 19:19:13 +0530

    Schemas and modules added

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 const app = require("./src/app");
+const mongoose = require('mongoose');
+
+
+
+
 const port = 3000;
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/FinalProject')
+.then(() => {console.log('Connected to the DB');})
+.catch((error) => {console.error("Error:", error)})
 
 app.listen( port, () => console.log("Server is running"));
--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const authController = require('./modules/auth/auth-controller');
 
+const addQuestionController = require("./routes/AddQuestions");
+const viewResult = require("./routes/ViewResult");
+const submitQuiz = require("./routes/SubmitQuiz");
+const getAttemptedQuizzes = require("./routes/GetQuizAttempt")
+const createQuiz = require("./routes/CreateQuiz");
+const createCandidate = require("./routes/CreateCandidate")
+
 app.use(express.json());
+app.use(cors());
 
 app.post('/auth/sign-in', authController.signInUser);
 
@@ -10,6 +19,12 @@ app.get('/', (req, res) => {
     res.send("hellllo")
 })
 
-// 
+// Lehaan
+app.use('/addQA', addQuestionController);
+app.use('/viewResult', viewResult);
+app.use('/attempt', submitQuiz);
+app.use('/getattempts', getAttemptedQuizzes);
+app.use('/newquiz', createQuiz);
+app.use('/candidate', createCandidate);
 
 module.exports = app;
