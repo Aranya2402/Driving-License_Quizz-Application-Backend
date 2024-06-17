ff3ced022a3e438b73d39d016c61e7d9e77488f4
Banujan Ravishankar <banujanr29@gmail.com>
2024-06-17 11:48:54 +0530

    stripe-integration-success

---
+++ src/modules/Payment/stripe-integration.js
@@ -0,0 +1,47 @@
+const stripe = require('stripe')('sk_test_51P0HbhKqUCwilBKSTuLI1ZJmlddnjBoGblIIQ4EYxM4RokAod2Nydm11i5di3d6VgfrPLx32rZ4wwhVUsIehRiSy00034cU2NL');
+const YOUR_DOMAIN = 'http://localhost:3000';
+
+
+const createCheckoutSession = async (req, res) => {
+  const { priceId } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      ui_mode: 'embedded',
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      return_url: `${YOUR_DOMAIN}/return?session_id={CHECKOUT_SESSION_ID}`,
+    });
+
+    res.send({ clientSecret: session.client_secret, id: session.id });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+const getSessionStatus = async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.retrieve(req.query.session_id);
+
+    if (!session.customer_details) {
+        return res.status(400).send({ error: "Customer details not found for this session" });
+      }
+
+    res.send({
+      status: session.status,
+      customer_email: session.customer_details.email
+    });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+module.exports = {
+  createCheckoutSession,
+  getSessionStatus
+};
